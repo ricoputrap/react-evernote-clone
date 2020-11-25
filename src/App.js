f6e0b406119eb2f82c01680b95bbba1ecfe98273
rico.putra <rico.putra@outlook.co.id>
2020-11-26 05:56:16 +0700

    PREPARE connection with firestore

--- src/App.js
+++ src/App.js
@@ -1,8 +1,36 @@
+import React from 'react';
 import './App.css';
+import firebase from 'firebase';
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      selectedNoteIndex: null,
+      selectedNote: null,
+      notes: null
+    };
+  }
+
   render() {
     return (<div>Hello World</div>)
+  }
+
+  componentDidMount = () => {
+    firebase
+      .firestore()
+      .collection('notes')
+      // the function that we pass into onSnapshot is going to automatically called 
+      //whenever the document or something inside the `notes` collection is updated
+      .onSnapshot(serverUpdate => {
+        const notes = serverUpdate.docs.map(doc => {
+          const data = doc.data();
+          data['id'] = doc.id;
+          return data;
+        });
+        console.log(notes)
+        this.setState({ notes: notes });
+      })
   }
 }
 
