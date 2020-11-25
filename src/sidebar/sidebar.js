fd88fda9bb2122c6d13255f396c02ec6a67c0a63
rico.putra <rico.putra@outlook.co.id>
2020-11-26 06:53:45 +0700

    SETUP add new note and cancel

--- src/sidebar/sidebar.js
+++ src/sidebar/sidebar.js
@@ -10,6 +10,10 @@ import SidebarItemComponent from '../sidebarItem/sidebarItem';
 class SidebarComponent extends React.Component {
     constructor() {
         super();
+        this.state = {
+            addingNote: false,
+            title: null
+        }
     }
 
     render () {
@@ -19,13 +23,30 @@ class SidebarComponent extends React.Component {
             <div className={classes.sidebarContainer}>
                 <Button
                     className={classes.newNoteBtn}
-                    onClick={this.newNoteBtnClick}>New Note</Button>
+                    onClick={this.newNoteBtnClick}>
+                    {this.state.addingNote ? 'Cancel' : 'New Note'}
+                </Button>
+                { this.state.addingNote && (
+                    <div>
+                        <input type="text"
+                            className={classes.newNoteInput}
+                            placeholder='Enter note title'
+                            onKeyUp={ e => this.updateTitle(e.target.value) } />
+                    </div>
+                )}
             </div>
         );
     }
 
     newNoteBtnClick = () => {
-        console.log("CLICKED")
+        this.setState({
+            title: null,
+            addingNote: !this.state.addingNote,
+        })
+    }
+
+    updateTitle = (txt) => {
+        console.log("TXT: " + txt)
     }
 }
 
